aaa1f0d1f019188999ff0fdd4b060a37f806f71b
YannDamon <damon@icmcb-bordeaux.cnrs.fr>
2014-02-04 18:05:08 +0100

    Commit 1
    Ajout de la gestion du serveur
    Ajout de la fonction connexion client
    Ajout de la fonction déconnexion client
    Ajout du menu statistiques
    Ajout d'un fichier statistiques (1.txt)

--- Bomberman/mainwindow.h
+++ Bomberman/mainwindow.h
@@ -2,6 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QVBoxLayout>
+#include <QtNetwork/QTcpServer>
+#include <QtNetwork/QTcpSocket>
+#include <QFile>
+#include <QTextEdit>
+#include <QString>
+#include <QTextStream>
+#include <QInputDialog>
+
 
 namespace Ui {
 class MainWindow;
@@ -27,9 +39,16 @@ private slots:
     void AudioSetting();
     void Help();
     void Credits();
+    void nouvelleConnexion();
+    void deconnexionClient();
+    void Statistics();
+
 
 private:
     Ui::MainWindow *ui;
+    QLabel *etatServeur;
+    QTcpServer *serveur; // Represente le serveur sur le reseau
+    QList<QTcpSocket *> clients; // Gere un tableau contenant la liste des clients
 };
 
 #endif // MAINWINDOW_H
